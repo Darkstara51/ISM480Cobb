01406372cd326f28558b3a23e2510bf7499ea1af
Darkstara51 <97412125+Darkstara51@users.noreply.github.com>
2024-02-23 22:24:02 -0700

    Add files via upload

---
+++ OOP Mini Project 2.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __init__(self, age, name, is_male, weight):
+        self.age = age
+        self.name = name
+        self.is_male = is_male # Boolean. True if Male, False if Female.
+        self.weight = weight
+
+my_dog = Dog(1, "moe", False, 5)
+
+print("Moe is a 5 pound female dog who is 1 year old.")
